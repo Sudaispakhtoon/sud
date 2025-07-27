b722a0262017dcddc8103377bdd45442a21031d9
Sudaispakhtoon <sudaiskhankhalil2010pk18@gmail.com>
2025-07-27 13:49:56 +0500

    Add files via upload
    This is a website of laptop shop

---
+++ SK.js
@@ -0,0 +1,31 @@
+var First="sudais"
+
+function Name() {
+    alert(First)
+alert(First)
+}
+
+var Second="It skill"
+
+function Skills() {
+    alert(Second)
+alert(Second)
+}
+
+var Third="Class 10 Student"
+
+function Education() {
+    alert(Third)
+alert(Third)
+}
+
+var Four="03*********"
+
+function ContactNo() {
+    alert(Four)
+alert(Four)
+}
+
+
+
+
